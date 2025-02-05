85d299e4f1d4e1800192ec13b7c60470e9ab33b7
micko112 <dimitrijemitic112@gmail.com>
2025-02-05 23:53:08 +0100

    Dodat nav bar sa tailwind, napravljeni view i Components, rute su postavljene, sredjen je nav bar preko nav-link i layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <h1>Welcome to about Page</h1>
+</x-layout>
+
---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+
+    <x-slot:heading>
+        Contacts Page
+    </x-slot:heading>
+    <h1>Welcome to Contacts Page</h1>
+</x-layout>
